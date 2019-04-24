8cfdfe74653a0ebbc0eccd071644cbd02db5ef54
Elisha Aywak <elishaaywak@gmail.com>
2019-04-24 12:15:06 +0300

    Aded the recepies page

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,4 +125,9 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+     public function actionRecepies()
+    {
+        return $this->render('recepies');
+    }
 }
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,10 +39,11 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
+            ['label' => 'About Food Poa', 'url' => ['/site/about']],
+            ['label' => 'Top Recepies', 'url' => ['/site/recepies']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
+                ['label' => 'Login/Register', 'url' => ['/site/login']]
             ) : (
                 '<li>'
                 . Html::beginForm(['/site/logout'], 'post')
@@ -69,7 +70,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Food Poa <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
